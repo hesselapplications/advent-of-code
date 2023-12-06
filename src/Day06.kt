6a4f2ed4af3e1f6ce70f02c910da701731591f0a
Noah Hessel <noah.hessel@datasite.com>
2023-12-06 09:44:36 -0700

    Day 6

--- src/Day06.kt
+++ src/Day06.kt
@@ -3,23 +3,18 @@ fun main() {
     val times = input.first().extractLongs()
     val recordDistances = input.last().extractLongs()
 
-    fun findWaysToBeat(time: Long, recordDistance: Long): Int {
-        var waysToWin = 0
-        for (buttonHoldTime in 1..<time) {
-            val remainingTime = time - buttonHoldTime
-            val distance = buttonHoldTime * remainingTime
-            if (distance > recordDistance) waysToWin++
+    fun waysToWin(time: Long, recordDistance: Long) = (1..<time)
+        .count { buttonHoldTime ->
+            buttonHoldTime * (time - buttonHoldTime) > recordDistance
         }
-        return waysToWin
-    }
 
     // Part 1
-    times.zip(recordDistances).map { (time, recordDistance) -> findWaysToBeat(time, recordDistance) }
+    times.zip(recordDistances).map { (time, recordDistance) -> waysToWin(time, recordDistance) }
         .reduce(Int::times)
         .println()
 
     // Part 2
     val time = times.joinToString("").toLong()
     val recordDistance = recordDistances.joinToString("").toLong()
-    findWaysToBeat(time, recordDistance).println()
+    waysToWin(time, recordDistance).println()
 }
