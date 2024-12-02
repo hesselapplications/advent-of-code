d4be2670687c5fa22cb339801e3f62b8c7fe4d76
Noah Hessel <noah.hessel@datasite.com>
2024-12-01 23:00:50 -0700

    Day 2

--- src/2024/Day02.kt
+++ src/2024/Day02.kt
@@ -5,32 +5,27 @@ import readInput
 import kotlin.math.abs
 
 fun main() {
-    val input = readInput("2024/Day02")
-
-    val reports = input.map { report ->
-        report.split(" ").map { it.toInt() }
+    val reports = readInput("2024/Day02").map {
+        it.split(" ").map(String::toInt)
     }
 
-    fun safeLevels(levels: List<Int>): Boolean {
-        val pairs = levels.zipWithNext() // Pair each level with the next
+    fun List<Int>.isSafe(): Boolean {
+        val pairs = this.zipWithNext() // Pair each level with the next
 
-        val safeDifference = pairs.all { (a, b) -> abs(a - b) in 1..3 } // All differences within 1-3
-        if (!safeDifference) return false
+        val unsafeDifference = pairs.any { (a, b) -> abs(a - b) !in 1..3 } // Any difference outside 1-3
+        if (unsafeDifference) return false
 
         return pairs.all { (a, b) -> a < b } // All increasing
                 || pairs.all { (a, b) -> a > b } // All decreasing
     }
 
     // Part 1
-    reports
-        .count { levels -> safeLevels(levels) }
-        .println()
+    reports.count { it.isSafe() }.println()
 
     // Part 2
     reports.count { levels ->
         levels.indices.any { index ->
-            val dampedLevels = levels.toMutableList().apply { removeAt(index) } // Remove one level
-            safeLevels(dampedLevels)
+            levels.toMutableList().apply { removeAt(index) }.isSafe() // Remove one level
         }
     }.println()
 
