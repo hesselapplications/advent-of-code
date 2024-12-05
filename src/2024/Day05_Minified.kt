1840ee4dad80f9d7126c19a1fa86243eb5614a8a
Noah Hessel <noah.hessel@datasite.com>
2024-12-05 12:32:19 -0700

    Day 5 Minified

--- src/2024/Day05_Minified.kt
+++ src/2024/Day05_Minified.kt
@@ -1,8 +1,8 @@
 import kotlin.io.path.*
 fun main() {
-    val i=Path("5").readLines();val r=i.filter{"|" in it}.map{it.split("|")};val u=i.filter{"," in it}.map{it.split(",")}
-    fun List<String>.b(n: String)=r.filter{it[1] == n&&it[0] in this&&it[1] in this}.map{it[0]}
-    fun Collection<List<String>>.m()=println(sumOf{it[it.size / 2].toInt()})
-    val c=u.filter{u->u.all{n->u.takeWhile{it!=n}.toSet()==u.b(n).toSet()}}.also{it.m()}
-    (u-c).map{it.sortedBy{n->it.b(n).size}}.m()
+val i=Path("5").readLines();val r=i.filter{"|" in it}.map{it.split("|")};val u=i.filter{"," in it}.map{it.split(",")}
+fun List<String>.b(n: String)=r.filter{it[1] == n&&it[0] in this&&it[1] in this}.map{it[0]}
+fun Collection<List<String>>.m()=println(sumOf{it[it.size / 2].toInt()})
+val c=u.filter{u->u.all{n->u.takeWhile{it!=n}.toSet()==u.b(n).toSet()}}.also{it.m()}
+(u-c).map{it.sortedBy{n->it.b(n).size}}.m()
 }
