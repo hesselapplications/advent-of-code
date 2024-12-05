3632a40b3fa06a31cfc078f55af7168b436c37a2
Noah Hessel <noah.hessel@datasite.com>
2024-12-05 12:52:04 -0700

    Day 5 Minified

--- src/2024/Day05_Minified.kt
+++ src/2024/Day05_Minified.kt
@@ -1,5 +1,5 @@
 import kotlin.io.path.*
-fun main() {
+fun main(){
 val i=Path("5").readLines();val r=i.filter{"|" in it}.map{it.split("|")};val u=i.filter{"," in it}.map{it.split(",")}
 fun List<String>.b(n: String)=r.filter{it[1] == n&&it[0] in this&&it[1] in this}.map{it[0]}
 fun Collection<List<String>>.m()=println(sumOf{it[it.size / 2].toInt()})
