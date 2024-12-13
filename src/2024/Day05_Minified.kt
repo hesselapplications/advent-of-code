9c6fa6dc21a1ee449dc4d2f1e5cf3f8ccc054399
Noah Hessel <noah.hessel@datasite.com>
2024-12-13 14:14:44 -0700

    Day 5

--- src/2024/Day05_Minified.kt
+++ src/2024/Day05_Minified.kt
@@ -1,11 +1,2 @@
 import java.io.File
-fun main(){
-val(r,p)=File("5").readText().split("\n\n")
-val a=mutableListOf(0,0)
-p.lines().forEach{
-val n=it.split(",")
-val s=n.sortedWith{x,y->if("$x|$y" in r)-1 else 1}
-a[if(n==s) 0 else 1]+=s[s.size/2].toInt()
-}
-println(a)
-}
+fun main(){val(r,p)=File("5").readText().split("\n\n");val a=mutableListOf(0,0);p.lines().forEach{val n=it.split(",");val s=n.sortedWith{x,y->if("$x|$y" in r)-1 else 1};a[if(n==s) 0 else 1]+=s[s.size/2].toInt()};println(a)}
